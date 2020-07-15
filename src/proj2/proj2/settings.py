84bb1607849b3cb59c554ac935ce6e65344a50d2
Matthew <matthewdunngeon@gmail.com>
2020-07-15 23:59:40 +0300

    Long time ago

--- src/proj2/proj2/settings.py
+++ src/proj2/proj2/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # Third Party
 
     # Own
+    'pages',
     'products',
 ]
 
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'proj2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- src/proj2/proj2/urls.py
+++ src/proj2/proj2/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+# from pages.views import home_view, contact_view, about_view, social_view, hello_there_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", include("pages.urls")),
+    # path('', home_view, name='home'),
+    # path('contact/', contact_view, name='contact'),
+    # path('about/', about_view, name='about'),
+    # path('social/', social_view, name='social'),
+    # path("hello/<name>", hello_there_view, name="hello_there"),
+    path('admin/', admin.site.urls), 
 ]

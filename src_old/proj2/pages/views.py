d4f51f0ed30f491917f989a38dfc717f859992f2
Matthew <matthewdunngeon@gmail.com>
2020-07-19 14:28:47 +0300

    sdf

--- src_old/proj2/pages/views.py
+++ src_old/proj2/pages/views.py
@@ -3,24 +3,31 @@ from datetime import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-def home_view(request, *args, **kwargs): #*args, **kwargs
+#  Create your views here.
+
+
+def home_view(request, *args, **kwargs):  # *args, **kwargs
     return HttpResponse("Hello, Django!")
 
-def contact_view(*args, **kwargs): #*args, **kwargs
+
+def contact_view(*args, **kwargs):  # *args, **kwargs
     return HttpResponse("<h1>Contact Page</h1>")
 
-def about_view(*args, **kwargs): #*args, **kwargs
+
+def about_view(*args, **kwargs):  # *args, **kwargs
     return HttpResponse("<h1>About Page</h1>")
 
-def social_view(*args, **kwargs): #*args, **kwargs
+
+def social_view(*args, **kwargs):  # *args, **kwargs
     return HttpResponse("<h1>Social Page</h1>")
+
 
 # for testing url
 print("http://127.0.0.1:8000/hello/Matthew\n")
 
+
 def hello_there_view(request, name):
-        return render(
+    return render(
         request,
         'pages/hello_there.html',
         {
@@ -29,12 +36,14 @@ def hello_there_view(request, name):
         }
     )
 
+
 def hello_there2_view(request, name):
     now = datetime.now()
     formatted_now = now.strftime("%A, %d %B, %Y at %X")
 
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
+    # Filter the name argument to letters only using regular expressions.
+    # URL arguments can contain arbitrary text, so we restrict to safe
+    # characters only.
     match_object = re.match("[a-zA-Z]+", name)
 
     if match_object:
